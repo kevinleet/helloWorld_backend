2ef80f678483cc5b522d0da0b8bec773d3a6987f
Josh Mathew <jsmathew41@gmail.com>
2023-06-30 12:28:14 -0500

    client server socket connection established

--- server.js
+++ server.js
@@ -2,36 +2,39 @@ const express = require("express");
 const Router = require("./routes/AppRouter");
 const cors = require("cors");
 const db = require("./db");
-const socketio = require("socket.io");
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const server = require("http").Server(app);
+//const server = require("http").Server(app);
 
-const io = socketio(server, {
-  cors: {
-    origin: "https://localhost:5173",
-    methods: ["GET", "POST"]
-  }
-});
+
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/api", Router);
 
-app.listen(PORT, () =>
+const server = app.listen(PORT, () =>
   console.log(`Application is listening on port ${PORT}.`)
 );
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: "https://localhost:5173",
+    methods: ["GET", "POST"]
+
+  }
+});
 
 io.on("connection", (socket) => {
   console.log(`Socket ${socket.id} connected`);
 
-  socket.on("sendMessage", (message) => {
-    io.emit("message", message);
-  });
+  // socket.on("sendMessage", (message) => {
+  //   io.emit("message", message);
+  // });
 
-  socket.on("disconnect", () => {
-    console.log(`Socket ${socket.id} disconnected`);
-  });
+  // socket.on("disconnect", () => {
+  //   console.log(`Socket ${socket.id} disconnected`);
+  // });
 });
