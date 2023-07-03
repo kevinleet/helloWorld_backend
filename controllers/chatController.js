254128f15d4a21da6cb5ff5812dacc3a124bacb4
Kevin Li <kevinli617@gmail.com>
2023-07-03 12:16:28 -0400

    add populate logic for getAllChats

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -2,7 +2,7 @@ const { Chat } = require("../models");
 
 const getAllChats = async (req, res) => {
   try {
-    let chats = await Chat.find().populate({path: 'users', ref: 'User'});
+    let chats = await Chat.find().populate({path: 'users'});
     res.json(chats);
   } catch (error) {
     res.send(error);
