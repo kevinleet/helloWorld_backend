d3ff47d0aa015d2f4199eb18d036dbfa6508cb79
Kevin Li <kevinli617@gmail.com>
2023-07-07 10:08:05 -0400

    added logic to prevent creation of multiple chats between the same two users

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -2,7 +2,7 @@ const { Chat, User } = require("../models");
 
 const getAllChats = async (req, res) => {
   try {
-    let chats = await Chat.find().populate({path: 'users'});
+    let chats = await Chat.find().populate({ path: "users" });
     res.json(chats);
   } catch (error) {
     res.send(error);
@@ -10,32 +10,44 @@ const getAllChats = async (req, res) => {
 };
 
 const getChatsByUser = async (req, res) => {
-  const { userId } = req.params
+  const { userId } = req.params;
   // console.log(userId)
   try {
     let chats = await Chat.find({ users: { $elemMatch: { $eq: userId } } })
-      .populate('users')
-      .populate({ path: 'latestMessage' , populate: { path: 'sender', select: 'email displayname' } });
-   
+      .populate("users")
+      .populate({
+        path: "latestMessage",
+        populate: { path: "sender", select: "email displayname" },
+      });
+
     // console.log('chats:', chats)
     // console.log('')
-    res.json(chats)
+    res.json(chats);
   } catch (error) {
-    res.send(error)
+    res.send(error);
   }
-}
+};
 
 const createChat = async (req, res) => {
   try {
-    let newChat = await (await Chat.create(req.body)).populate('users', {select: 'displayname'})
-    res.send(newChat)
+    let existingChat = await Chat.findOne({
+      users: { $all: [req.body.user1, req.body.user2] },
+    });
+    if (existingChat) {
+      res.send(existingChat);
+    } else {
+      let newChat = await (
+        await Chat.create({ users: [req.body.user1, req.body.user2] })
+      ).populate("users", { select: "displayname" });
+      res.send(newChat);
+    }
   } catch (error) {
-    res.send(error)
+    res.send(error);
   }
-}
+};
 
 module.exports = {
   getAllChats,
   getChatsByUser,
-  createChat
+  createChat,
 };
